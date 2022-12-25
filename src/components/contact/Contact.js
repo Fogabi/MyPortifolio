8b24556264ace7efeca2701aaf5e16365d04fc51
Fogabi <gabifogaca.gabriela@gmail.com>
2022-12-25 15:37:43 -0300

    Adding GitHub button

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,6 +3,7 @@ import "./Contact.css";
 import whatsapp from "../../img/contact/whatsapp.png";
 import email from "../../img/contact/email.png";
 import linkedin from "../../img/contact/linkedin.png";
+import github from '../../img/contact/github.png'
 
 function Contact() {
   return (
@@ -17,8 +18,8 @@ function Contact() {
               </p>
               <p className="hire__text white">
                 <strong>gabifogaca.gabriela@gmail.com</strong> or
-                WhatsApp/Telegram: <strong>+55 12 98217-1835</strong>. Or click on
-                the icons to be directed directly to my contact:
+                WhatsApp/Telegram: <strong>+55 12 98217-1835</strong>. Or click
+                on the icons to be directed directly to my contact:
               </p>
             </div>
             <div className="div__icons">
@@ -30,6 +31,9 @@ function Contact() {
               </a>
               <a href="https://www.linkedin.com/in/gabriela-foga%C3%A7a-864261174/">
                 <img src={linkedin} className="contact__icon"></img>
+              </a>
+              <a href="https://github.com/Fogabi">
+                <img src={github} className="contact__icon"></img>
               </a>
             </div>
           </div>
